701026f5278e83057c4414cfa904c60fe208614b
fabaodevudi <fabaodevudi@gmail.com>
2022-10-30 21:35:50 -0300

    Remoção

--- src/main/java/com/fabao/operadora/saude/repository/AutorizacaoRepositoryFake.java
+++
@@ -1,7 +0,0 @@
-package com.fabao.operadora.saude.repository;
-
-import com.fabao.operadora.saude.entity.Autorizacao;
-
-public interface AutorizacaoRepositoryFake  {
-	Autorizacao save(Autorizacao entity);
-}
